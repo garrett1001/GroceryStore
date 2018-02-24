71d749e96cf51af08524b134e21d0a6b41bade36
garrett1001 <gwelton@mail.csuchico.edu>
2018-02-23 21:46:50 -0800

    menu repeats

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Author: Garrett Welton
 #include<iostream>
 #include<string>
 using namespace std;
@@ -6,14 +7,31 @@ int main()
 {
   string list[5]; //array of 5 strings
   int numItems = 0;
-  char input;
+  char input = 0;
 
+while(i<1)
+{
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+
+  if(input=='A' || input=='a')
+  {
+  }
+
+  else if(input=='Q' || input=='q')
+  {
+  i++;
+  }
+
+  else
+  {
+  }
+}
+
 
   return 0;
 }
