9601ffee73414601d8a727bc6c0533a4c30dd51a
garrett1001 <gwelton@mail.csuchico.edu>
2018-02-27 21:50:10 -0800

    Complete

--- main.cpp
+++ main.cpp
@@ -5,49 +5,54 @@ using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input = 0;
-  int i = 0;
-  string item;
+        string list[5]; //array of 5 strings
+        int numItems = 0;
+        char input = 0;
+        int i = 0;
+        string item;
 
-while(i<1)
-{
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+        while(i<1)
+        {
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
 
-  if(input=='A' || input=='a')
-  {
-   cout<<"What is the item?"<<endl;
-   cin>>item;
+                if(input=='A' || input=='a')
+                {
+                        cout<<"What is the item?"<<endl;
+                        cin>>item;
 
-    if(numItems<5)
-    {
-     list[numItems] = item;
-     numItems++;
-    }
+                        if(numItems<5)
+                        {
+                                list[numItems] = item;
+                                numItems++;
+                        }
 
-    else
-    {
-     cout<<"You'll need a bigger list!"<<endl;
-    }
-  }
+                        else
+                        {
+                                cout<<"You'll need a bigger list!"<<endl;
+                        }
+                }
 
-  else if(input=='Q' || input=='q')
-  {
-  i++;
-  }
+                else if(input=='Q' || input=='q')
+                {
+                        cout<<"==ITEMS TO BUY=="<<endl;
+                        for (int x=0; x<5; x++)
+                        {
+                                cout<<x+1<<" "<<list[x]<<endl;
+                        }
+                        i++;
+                }
 
-  else
-  {
-  cout<<"Invalid selection"<<endl;
-  }
-}
+                else
+                {
+                        cout<<"Invalid selection"<<endl;
+                }
+        }
 
 
-  return 0;
+        return 0;
 }
